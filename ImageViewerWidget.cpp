d648f5363c1dbf3c6ad5fd479d09120f0fa4decd
Gluzix <kamil.gluza@gmail.com>
2022-12-04 21:56:19 +0100

    add save image as .png .jpg

--- ImageViewerWidget.cpp
+++ ImageViewerWidget.cpp
@@ -2,6 +2,7 @@
 #include "ui_ImageViewerWidget.h"
 #include <QFileDialog>
 #include <QPixmap>
+#include <QImage>
 #include <QDebug>
 
 ImageViewerWidget::ImageViewerWidget(QWidget *parent)
@@ -11,15 +12,31 @@ ImageViewerWidget::ImageViewerWidget(QWidget *parent)
     ui->setupUi(this);
     connect(ui->uploadButton, &QPushButton::clicked, this,
             &ImageViewerWidget::onUploadButtonClicked);
+
+    connect(ui->saveButton, &QPushButton::clicked, this,
+            &ImageViewerWidget::onSaveButtonClicked);
 }
 
 void ImageViewerWidget::onUploadButtonClicked()
 {
-    QString path = QFileDialog::getOpenFileName(this, "", "", "");
-    QPixmap pixmap(path);
+    QString path = QFileDialog::getOpenFileName(this, "", "", "Image Files (*.png *.jpg)");
 
-    QPixmap scaled = pixmap.scaled(ui->imageViewerLabel->size(), Qt::KeepAspectRatio);
-    ui->imageViewerLabel->setMinimumSize(scaled.size());
-    ui->imageViewerLabel->setMaximumSize(scaled.size());
-    ui->imageViewerLabel->setPixmap(scaled);
+    if (!path.isEmpty()) {
+        QPixmap pixmap(path);
+
+        QPixmap scaled = pixmap.scaled(ui->imageViewerLabel->size(), Qt::KeepAspectRatio);
+        ui->imageViewerLabel->setMinimumSize(scaled.size());
+        ui->imageViewerLabel->setMaximumSize(scaled.size());
+        ui->imageViewerLabel->setPixmap(scaled);
+    }
+}
+
+void ImageViewerWidget::onSaveButtonClicked()
+{
+    QString path = QFileDialog::getSaveFileName(this, "", "", "Image Files (*.png *.jpg)");
+
+    if (!path.isEmpty()) {
+        QImage image = ui->imageViewerLabel->getCroppedPixmap().toImage();
+        image.save(path);
+    }
 }
